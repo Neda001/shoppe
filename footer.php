5589cd18a657fecc1e2199bcb34e414341f812e2
Neda <ariangirl.ee@gmail.com>
2023-04-23 21:05:20 +0200

    message

---
+++ footer.php
@@ -0,0 +1,38 @@
+<footer>
+        <div class="footer-left">
+          <div class="down-nav">
+            <ul>
+              <li><a href="">contact</a></li>
+              <li><a href="">terms of services</a></li>
+              <li><a href="">shipping and returns</a></li>
+            </ul>
+          </div>
+          <div class="rules">
+            <span>(c)2021 Shelly. </span>
+            <span> Terms of use <b>and</b> privacy policy </span>
+          </div>
+        </div>
+        <div class="footer-right">
+          <div class="newsletter">
+            <input type="text" placeholder="Give an email,get the newsletter" />
+            <button type="submit">
+              <img src="./images/Icon color (4).png" alt="" />
+            </button>
+          </div>
+
+          <div class="icon-down">
+            <li>
+              <a href=""><img src="./images/Icon color.png" alt="" /></a>
+            </li>
+            <li>
+              <a href=""><img src="./images/Icon color (1).png" alt="" /></a>
+            </li>
+            <li>
+              <a href=""><img src="./images/Icon color (2).png" alt="" /></a>
+            </li>
+            <li>
+              <a href=""><img src="./images/Icon color (3).png" alt="" /></a>
+            </li>
+          </div>
+        </div>
+      </footer>
